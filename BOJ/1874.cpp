8dbf5a783f558ef43f58ddc91cda85d326267ddf
Cheol Seong Park <tjd4987@naver.com>
2017-09-03 15:57:53 +0900

    implementation

---
+++ BOJ/1874.cpp
@@ -0,0 +1,36 @@
+//category : implementation
+
+#include <stdio.h>
+#include <stack>
+#include <vector>
+using namespace std;
+int a[100000];
+int main(){
+	int i;
+	int n;
+	int cnt = 1;
+	vector<char> v;
+	stack<int> st;
+	scanf("%d",&n);
+	for (i = 0; i < n; i++){
+		scanf("%d",a+i);
+	}
+	for (i = 0; i < n; i++){
+		while (st.empty()||st.top()<a[i]){
+			st.push(cnt++);
+			v.push_back('+');
+		}
+		if (st.top() == a[i]){
+			st.pop();
+			v.push_back('-');
+		}
+		else{
+			v.clear();
+			break;
+		}
+	}
+	if (v.empty()) return printf("NO");
+	for (i = 0; i < v.size(); i++)
+		printf("%c\n",v[i]);
+	return 0;
+}
