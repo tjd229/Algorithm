cd8aa4a4bdb5b98690a458dc9e97a79daddc7df7
Cheol Seong Park <tjd4987@naver.com>
2017-10-05 15:50:55 +0900

    AC, string

--- BOJ/9250.cpp
+++ BOJ/9250.cpp
@@ -2,109 +2,93 @@
 
 
 
-#include <stdio.h>
-#include <string>
 #include <iostream>
-#include <queue>
 #include <vector>
+#include <queue>
+#include <string>
+#define pb push_back
 using namespace std;
 bool judge;
-struct AC{
-	struct Node{
-		int pi;
-		int edges[26];
-		bool valid;
-		vector<int> ixes;
-		Node(){
-			pi = 0;
-			valid = false;
-			for (int i = 0; i < 26; i++) edges[i] = 0;
-		}
-	};
-	vector<Node> nodes;
-	int ix;
-	void init(){
-		Node n;
-		nodes.push_back(n);
-		ix = 1;
+int cnt;
+struct Node{
+	int pi;
+	int edge[26];
+	bool valid;
+	vector<int> tail;
+}node[(int)1e5+1];
+void make_tree(int ix,string &s,int curr,int e){
+	if (curr == e){
+		node[ix].valid = true;
+		return;
 	}
-	void insert(int root, string s, int curr,int e){
-		if (curr == e){
-			nodes[root].valid = true;
-			return;
-		}
-		if (nodes[root].edges[s[curr]-'a'] == 0){
-			nodes[root].edges[s[curr] - 'a'] = ix;
-			Node n;
-			nodes.push_back(n);
-			ix++;
-		}
-		insert(nodes[root].edges[s[curr] - 'a'], s, curr + 1, e);
+	if (node[ix].edge[s[curr] - 'a'] == 0){
+		node[ix].edge[s[curr] - 'a'] = ++cnt;
 	}
-	void set_pi(int s){
-		queue<int> q;
-		int i,j,k,p;
-		q.push(s);
-		while (!q.empty()){
-			i = q.front();
-			q.pop();
-			for (j = 0; j < 26; j++){
-				if (nodes[i].edges[j] != 0){
-					k = nodes[i].edges[j];
-					p = nodes[i].pi;
-					while (p>0 && nodes[p].edges[j] == 0) p = nodes[p].pi;
-					if (k != nodes[p].edges[j]){
-						nodes[k].pi = nodes[p].edges[j];
-						nodes[k].ixes = nodes[nodes[k].pi].ixes;
-						if (nodes[nodes[k].pi].valid) nodes[k].ixes.push_back(nodes[k].pi);
-					}
-					q.push(k);
+	make_tree(node[ix].edge[s[curr] - 'a'], s, curr + 1, e);
+}
+void set_pi(int s){
+	queue<int> q;
+	q.push(s);
+	int curr,nxt;
+	int i;
+	int p;
+	while (!q.empty()){
+		curr = q.front();
+		q.pop();
+		for (i = 0; i < 26; i++){
+			if (node[curr].edge[i] != 0){
+				nxt = node[curr].edge[i];
+				p = node[curr].pi;
+				while (p > 0 && node[p].edge[i] == 0)
+					p = node[p].pi;
+				if (nxt != node[p].edge[i]){
+					node[nxt].pi = node[p].edge[i];
+					node[nxt].tail = node[node[nxt].pi].tail;
+					if (node[node[nxt].pi].valid)
+						node[nxt].tail.pb(node[nxt].pi);
 				}
-			}			
+				q.push(nxt);
+			}
 		}
 	}
-	void search(int root, string s, int curr, int e){
-		while (curr != e){
-			
-			if (nodes[root].edges[s[curr] - 'a'] != 0){
-				if (nodes[root].ixes.size() != 0){
-					judge = true;
-					return;
-				}
-				root = nodes[root].edges[s[curr] - 'a'];
-				curr++;
-			}
-			else{
-				if (root == 0) curr++;
-				root = nodes[root].pi;
-			}
-			if (nodes[root].valid || nodes[root].ixes.size() != 0){
+	
+}
+void search(int ix, string &s, int curr, int e){
+	while (curr != e){
+		if (node[ix].edge[s[curr] - 'a'] != 0){
+			if (node[ix].tail.size() != 0){
 				judge = true;
 				return;
 			}
+			ix = node[ix].edge[s[curr] - 'a'];
+			curr++;
 		}
-		
+		else{
+			if (ix == 0) curr++;
+			ix = node[ix].pi;
+		}
+		if (node[ix].valid || node[ix].tail.size() != 0){
+			judge = true;
+			return;
+		}
 	}
-	
-};
+}
 int main(){
-	int N,Q;
+	int N, Q;
 	string s;
-	AC ac;
-	scanf("%d", &N);
-	ac.init();
-	do{
+	cin >> N;
+	while (N--){
 		cin >> s;
-		ac.insert(0,s,0,s.size());
-	} while (--N);
-	ac.set_pi(0);
-
-	for (scanf("%d", &Q); Q--;){
+		make_tree(0,s,0,s.size());
+	}
+	set_pi(0);
+	cin >> Q;
+	
+	while (Q--){
 		cin >> s;
 		judge = false;
-		ac.search(0,s,0,s.size());
-		printf("%s\n",judge? "YES":"NO");
+		search(0, s, 0, s.size());
+		cout << (judge ? "YES\n" : "NO\n");
 	}
-	
 	return 0;
 }
