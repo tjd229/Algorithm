dfb4f0d644f27c7b1348c5f87ab6ef09fd31b81d
Cheol Seong Park <tjd4987@naver.com>
2018-08-12 20:02:11 +0900

    graph

--- BOJ/1199_adjMat.cpp
+++ BOJ/1199_adjMat.cpp
@@ -5,21 +5,15 @@
 using namespace std;
 int graph[1001][1001];
 int degree[1001];
-int ix[1001];
 int N,M;
 vector<int> ans;
 int dfs(int n){
 	int i;
 	int m=0;
-	//for(i=1;i<=N;i++){
-	for(;ix[n]<=N;){
-		i=ix[n];
-		if(graph[n][i]==0)
-			ix[n]++;
+	for(i=1;i<=N;i++){
 		if(graph[n][i]>0){
 			graph[n][i]--;
 			graph[i][n]--;
-			
 			m+=dfs(i)+1;
 		}
 	}
@@ -38,19 +32,18 @@ int main(){
 				M+=graph[i][j];
 			}
 		}
-		ix[i]=1;
 	}
 	//M=20;
 	M/=2;
 	for(i=1;i<=N;i++){
 		if(degree[i]%2!=0){
 			printf("-1");
-			return -1;
+			return 0;
 		}
 	}
 	if(dfs(1)!=M){
 		printf("-1");
-		return -1;
+		return 0;
 	}
 	//dfs(1);
 	for(i=0;i<ans.size();i++){
