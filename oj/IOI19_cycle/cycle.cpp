32cac59740bcfc84a85a285fa7c88746dc8c110c
Cheol Seong Park <tjd4987@naver.com>
2019-08-24 20:11:12 +0900

    bs

---
+++ oj/IOI19_cycle/cycle.cpp
@@ -0,0 +1,34 @@
+//bs
+#include "cycle.h"
+void escape(int n) {
+	int half = n >> 1;
+	int prev = 0;
+	if (jump(0)) {
+		int l = 1, r = half;
+		int to0 = -1;
+		while (l <= r) {
+			int m = (l + r) >> 1;
+			if (jump((n - prev + m) % n)) {
+				to0 = m;
+				l = m + 1;
+			}
+			else r = m - 1;
+			prev = m;
+		}
+		jump((n-prev+to0)%n);
+	}
+	else {
+		int l = 1, r = half;
+		int toM = 1;
+		while (l <= r) {
+			int m = (l + r) >> 1;
+			if (jump((n - prev + m) % n)) r = m - 1;
+			else {
+				toM = m + 1;
+				l = m + 1;
+			}
+			prev = m;
+		}
+		jump((n - prev + toM+half) % n);
+	}
+}
