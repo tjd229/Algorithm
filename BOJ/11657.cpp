5f8911ab7d90918f14eb02eb6e0aa5933a030651
Cheol Seong Park <tjd4987@naver.com>
2020-10-03 20:39:56 +0900

    graph, SPFA

--- BOJ/11657.cpp
+++ BOJ/11657.cpp
@@ -1,35 +1,53 @@
-//category : graph, Bellman Ford
+//graph, SPFA
 
 #include <stdio.h>
 #include <vector>
-#define MAXVAL 10000000
+#include <queue>
+#define pii pair<int,int>
 using namespace std;
-int d[501];
-struct Edge{
-	int u,v,c;
-};
-Edge e[6000];
-int main(){
-	int N,M;
-	int i,j;
-	scanf("%d%d",&N,&M);
-	for(i=0;i<M;i++){
-		scanf("%d%d%d",&e[i].u,&e[i].v,&e[i].c);
+const int inf = 1e9;
+long long d[501];
+int upd[501];
+int mat[501][501];
+bool inq[501];
+vector<pii > edge[501];
+int main() {
+	int i, N, M;
+	for (scanf("%d%d", &N, &M), i = 1; i <= N; ++i) {
+		d[i] = inf;
+		for (int j = 1; j <= N; ++j) mat[i][j]=inf;
 	}
-	for(i=2;i<=N;i++){ 
-		d[i]=MAXVAL;
+	for (i = 0; i < M; ++i) {
+		int A, B, C; scanf("%d%d%d",&A,&B,&C);
+		if (mat[A][B] > C) mat[A][B] = C;
 	}
-	for(i=0;i<N;i++){
-		for(j=0;j<M;j++){
-			if(d[e[j].u]!=MAXVAL&&d[e[j].v]>d[e[j].u]+e[j].c){
-				if(i==N-1) return !printf("-1");
-				d[e[j].v]=d[e[j].u]+e[j].c;
+	for (i = 1; i <= N; ++i) for (int j = 1; j <= N; ++j) if (mat[i][j] < inf) edge[i].push_back({j,mat[i][j]});
+	queue<int> q; q.push(1);
+	d[1] = 0;
+	inq[1] = 1;
+	bool ans = 1;
+	while (q.size() && ans) {
+		int x = q.front(); q.pop();
+		inq[x] = 0;
+		for (auto p : edge[x]) {
+			int nxt = p.first, w = p.second;
+			if (d[nxt] > d[x] + w) {
+				d[nxt] = d[x] + w;
+				if (inq[nxt] == 0) {
+					inq[nxt] = 1;
+					q.push(nxt);
+					if (++upd[nxt] == N) {
+						ans = 0;
+						break;
+					}
+				}
+				
 			}
 		}
 	}
-	for(i=2;i<=N;i++){ 
-		if(d[i]==MAXVAL) d[i]=-1;
-		printf("%d\n",d[i]);
+	if (!ans) printf("-1");
+	else {
+		for (i = 2; i <= N; ++i) printf("%d\n",d[i]==inf? -1:d[i]);
 	}
-
+	return 0;
 }
