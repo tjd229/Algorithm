0288b5744c676a921417890bdbd42e75ac4af5e5
Cheol Seong Park <tjd4987@naver.com>
2020-10-03 20:58:24 +0900

    graph

--- BOJ/5014.cpp
+++ BOJ/5014.cpp
@@ -1,32 +1,41 @@
-//category : graph
+//graph
 
 #include <stdio.h>
-#include <vector>
+#include <queue>
 using namespace std;
 int F, S, G, U, D;
-int move(int x,int p,int m){
-	int M = 0;
-	int d;
-	for (int i = 0;i<=1000000; i++,M+=m){
-		d = (x + M) / p;
-		if (p*d == x + M) return d + i;
+int mv[1000001];
+int main() {
+
+	scanf("%d%d%d%d%d", &F, &S, &G, &U, &D);
+	if (S == G) {
+		printf("0");
+		return 0;
 	}
-	return -1;
-}
-
-
-int main(){
-	
-	scanf("%d%d%d%d%d",&F,&S,&G,&U,&D);
-	if (S == G) return !printf("0");
-	if (S<G&&U<1) return !printf("use the stairs");
-	if (S>G&&D<1) return !printf("use the stairs");
-	int ans;
-	if (G - S > 0) ans = move(G - S, U, D);
-	if (G - S < 0) ans = move(S-G, D, U);
-	if (ans<0) printf("use the stairs");
-	else printf("%d",ans);
-
+	if (S < G&&U < 1) {
+		printf("use the stairs");
+		return 0;
+	}
+	if (S > G&&D < 1) {
+		printf("use the stairs");
+		return 0;
+	} 
+	queue<int> q; q.push(S);
+	while (q.size()) {
+		int x = q.front(); q.pop();
+		int u = x + U;
+		if (u != S && u <= F && mv[u] == 0) {
+			mv[u] = mv[x] + 1;
+			q.push(u);
+		}
+		int d = x - D;
+		if (d != S && d > 0 && mv[d] == 0) {
+			mv[d] = mv[x] + 1;
+			q.push(d);
+		}
+	}
+	if(mv[G]==0) printf("use the stairs");
+	else printf("%d",mv[G]);
 
 	return 0;
 }
