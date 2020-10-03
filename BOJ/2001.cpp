7001c72ba5600c679dc647c89e4820a67524154d
Cheol Seong Park <tjd4987@naver.com>
2020-10-03 19:09:04 +0900

    graph

--- BOJ/2001.cpp
+++ BOJ/2001.cpp
@@ -1,62 +1,57 @@
 //graph
+#include <stdio.h>
 #include <vector>
 #include <queue>
-#include <stdio.h>
 #define pii pair<int,int>
 using namespace std;
+bool vis[101][1 << 14];
 int gem[101];
 vector<pii > edge[101];
-int vis[101][1 << 14];
-int bit_cnt(int n) {
+int _pop_count(int x) {
 	int cnt = 0;
-	for (; n; n -= (n&-n))++cnt;
+	for (; x; x -= (x&-x)) ++cnt;
 	return cnt;
 }
 int bfs() {
 	int ans = 0;
-	vis[1][0] = 1;
 	queue<pii > q;
 	q.push({1,0});
-
+	vis[1][0] = 1;
 	while (q.size()) {
-		int cur = q.front().first;
+		int x = q.front().first;
 		int stat = q.front().second;
-		int n = bit_cnt(stat);
+		int cnt = _pop_count(stat);
 		q.pop();
-		if (cur == 1 && ans < n) ans = n;
-		for (auto p : edge[cur]) {
-			if (p.second < n) continue;
-			int nxt = p.first;
-			if (vis[nxt][stat] == 0) {
+		if (x == 1 && ans < cnt) ans = cnt;
+		for (auto p : edge[x]) {
+			int nxt = p.first, c = p.second;
+			if (vis[nxt][stat] == 0 && cnt<=c) {
 				vis[nxt][stat] = 1;
 				q.push({nxt,stat});
 			}
-			if (gem[nxt]) {
-				int nstat = stat | (1 << (gem[nxt]-1));
-				if (p.second < n + 1) continue;
-				if (vis[nxt][nstat] == 0) {
-					vis[nxt][nstat] = 1;
-					q.push({nxt,nstat});
+			if (gem[x]) {
+				int nxt_stat = stat | (1 << (gem[x] - 1));				
+				if (vis[x][nxt_stat] == 0 ) {
+					vis[x][nxt_stat] = 1;
+					q.push({ x,nxt_stat });
 				}
 			}
-			
 		}
 	}
-	return ans+(gem[1]>0);
+	return ans;
 }
 int main() {
-	int i;
-	int n, m, K;
-	int a, b, c;
-	for (scanf("%d%d%d", &n, &m, &K),i=1;i<=K;++i) {
-		scanf("%d",&a);
-		gem[a] = i;
+	int i, n, m, K;
+	for (scanf("%d%d%d", &n, &m, &K), i = 0; i < K; ++i) {
+		int isle; scanf("%d",&isle);
+		gem[isle] = i+1;
 	}
-	while (m--) {
-		scanf("%d%d%d",&a,&b,&c);
+	for (i = 0; i < m; ++i) {
+		int a, b, c; scanf("%d%d%d",&a,&b,&c);
 		edge[a].push_back({b,c});
 		edge[b].push_back({ a,c });
-	}
-	printf("%d",bfs());
+	}printf("%d",bfs());
+	
+
 	return 0;
 }
