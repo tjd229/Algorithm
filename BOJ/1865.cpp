1ab742374d97009fd692fe555a33aecd37cb5ba1
Cheol Seong Park <tjd4987@naver.com>
2020-10-03 20:13:43 +0900

    graph, SPFA

--- BOJ/1865.cpp
+++ BOJ/1865.cpp
@@ -1,79 +1,88 @@
-//category : graph, SPFA
+//graph, SPFA
 
 
-#include <stdio.h>
 #include <vector>
+#include <stdio.h>
 #include <queue>
+#define pii pair<int,int>
 using namespace std;
-#define MAXVAL (2e9)
-struct Node{
-	int d;
-	int c;
-	bool in;
-	vector<pair<int, int> >edges;
-	void init(){
-		c = 0;
-		d = MAXVAL;
-		edges.clear();
-		in = false;
+const int inf = 2e9;
+int mat[501][501];
+int upd[501];
+int d[501];
+bool in[501];
+bool vis[501];
+vector<pii > edge[501];
+void dfs(int x,int from) {
+	if (vis[x]) return;
+	vis[x] = 1;
+	for (auto p : edge[x]) {
+		int nxt = p.first;
+		if (nxt == from) continue;
+		dfs(nxt, x);
 	}
-};
-Node pt[501];
-int main(){
-	int T;
-	int N, M, W;
-	int S, E, H;
-	int i;
-		
-	
-	pair<int, int> p;
-	for (scanf("%d", &T); T--;){
-		scanf("%d%d%d",&N,&M,&W);
-		for (i = 1; i <= N; i++) pt[i].init();
-		for (i = 0; i < M; i++){
-			scanf("%d%d%d",&S,&E,&H);
-			pt[S].edges.push_back(make_pair(E, H));
-			pt[E].edges.push_back(make_pair(S, H));
-			
+}
+int main() {
+	int TC;
+	for (scanf("%d", &TC); TC--;) {
+		int i, N, M, W;
+		for (scanf("%d%d%d", &N, &M, &W), i = 1; i <= N; ++i) {
+			upd[i] = 0;
+			in[i] = 0;
+			vis[i] = 0;
+			d[i] = inf;
+			edge[i].clear();
+			for (int j = 1; j <= N; ++j) mat[i][j] = mat[j][i] = inf;
 		}
-		for (i = 0; i < W; i++){
-			scanf("%d%d%d", &S, &E, &H);
-			pt[S].edges.push_back(make_pair(E, -H));			
+		for (i = 0; i < M; ++i) {
+			int S, E, T; scanf("%d%d%d",&S,&E,&T);
+			if (mat[S][E] > T)
+				mat[S][E] = mat[E][S] = T;
+		}
+		for (i = 1; i <= N; ++i) {
+			for (int j = 1; j <= N; ++j) {
+				if (mat[i][j] < inf) edge[i].push_back({ j,mat[i][j] });
+			}
+		}
+		//dfs
+		vector<int> src;
+		for (i = 1; i <= N; ++i) {
+			if (vis[i] == 0) {
+				dfs(i,0);
+				src.push_back(i);
+			}
+		}
+		for (i = 0; i < W; ++i) {
+			int S, E, T; scanf("%d%d%d", &S, &E, &T);
+			edge[S].push_back({E,-T});
 		}
 		queue<int> q;
-		q.push(1);
-		pt[1].d = 0;
-		pt[1].c++;
-		pt[1].in = true;
-		H = 0;
-		while (!q.empty()&&H==0){
-			S = q.front();
-			q.pop();
-			pt[S].in = false;
-			for (i = 0; i < pt[S].edges.size(); i++){
-				p = pt[S].edges[i];
-				E = p.first;
-				if (pt[S].d != MAXVAL&&pt[E].d>pt[S].d + p.second){
-					pt[E].d = pt[S].d + p.second;
-					
-					if (!pt[E].in){
-						pt[E].c++;
-						q.push(E);
-
-						pt[E].in = true;
+		for (auto x : src) {
+			q.push(x);
+			in[x] = 1;
+			d[x] = 0;
+		}
+		bool can = 0;
+		while (q.size() && can==0) {
+			int x = q.front(); q.pop();
+			in[x] = 0;
+			for (auto p : edge[x]) {
+				int nxt = p.first, w = p.second;
+				if (d[nxt] > d[x] + w) {
+					d[nxt] = d[x] + w;
+					if (in[nxt] == 0) {
+						in[nxt] = 1;
+						q.push(nxt);
 					}
-					if (pt[E].c >= N){
-						H = 1;
+					if (++upd[nxt]==N) {
+						can = 1;
 						break;
 					}
 				}
 			}
-
 		}
-		printf("%s\n",H==1? "YES":"NO");
+		printf("%s\n",can? "YES":"NO");
 	}
-
 
 	return 0;
 }
-
