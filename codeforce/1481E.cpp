123ac48faaaab8696da5452bd1b0bf0b18a4ecf4
Cheol Seong Park <tjd4987@naver.com>
2021-02-14 22:16:21 +0900

    에

---
+++ codeforce/1481E.cpp
@@ -0,0 +1,22 @@
+//dp
+#include <stdio.h>
+int l[500001], r[500001];
+int suf[500001];
+int a[500001],d[500002];
+int main() {
+	int i, n;
+	for (scanf("%d", &n), i = 1; i <= n; ++i) scanf("%d",a+i);
+	for (i = 1; i <= n; ++i) r[a[i]] = i;
+	for (i = n; i > 0; --i) l[a[i]] = i;
+	for (i = n; i > 0; --i) {
+		++suf[a[i]];
+		d[i] = d[i + 1];
+		if (i != l[a[i]] && d[i] < suf[a[i]])
+			d[i] = suf[a[i]];
+		if (i == l[a[i]] && d[i] < d[r[a[i]]+1]+suf[a[i]])
+			d[i] = d[r[a[i]] + 1] + suf[a[i]];
+	}
+	printf("%d",n-d[1]);
+	
+	return 0;
+}
