d8e86dc9ca2460814dbfcb7bb198ff63421ebcde
Cheol Seong Park <tjd4987@naver.com>
2021-05-23 17:19:36 +0900

    math, greedy

---
+++ GCJ/Append Sort.cpp
@@ -0,0 +1,83 @@
+//https://codingcompetitions.withgoogle.com/codejam/round/000000000043585d/00000000007549e5
+//math, greedy
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int X[101];
+void decomp(int x,vector<int> &v) {
+	while (x) {
+		v.push_back(x%10);
+		x /=10;
+	}
+	reverse(v.begin(), v.end());
+}
+int inc(vector<int> &pre, vector<int> &cur) {
+	if (pre.size() < cur.size()) return 0;
+	else if (pre.size() == cur.size()) {
+		for (int i = 0; i < pre.size(); ++i) {
+			if (pre[i] < cur[i]) return 0;
+			else if (pre[i] > cur[i]) 
+				break;
+			
+		}
+		cur.push_back(0);
+		return 1;
+	}
+	else {
+		int coin = 0;
+		for (int i = 0; i < cur.size(); ++i) {
+			if (pre[i] < cur[i]) {
+				coin = pre.size() - cur.size();
+				break;
+			}
+			else if (pre[i] > cur[i]) {
+				coin = pre.size() - cur.size() + 1;
+				break;
+			}
+		}
+		if (coin) {
+			for (int i = 0; i < coin; ++i)
+				cur.push_back(0);
+			return coin;
+		}
+		vector<int> add;
+		for (int i = cur.size(); i < pre.size(); ++i) 
+			add.push_back(pre[i]);
+		int carry = 1;
+		for (int i = add.size() - 1; i >= 0 && carry; --i) {
+			++add[i];
+			if (add[i] == 10) {
+				add[i] = 0;
+				carry = 1;
+			}
+			else carry = 0;
+		}
+		if (carry) {
+			for (int i = 0; i < add.size()+1; ++i)
+				cur.push_back(0);
+			return add.size() + 1;
+		}
+		for (auto x : add) cur.push_back(x);
+		return add.size();
+	}
+
+}
+int main() {
+	int tc, T;
+	for (scanf("%d", &T), tc = 1; tc <= T; ++tc) {
+		int i, N;
+		int ans = 0;
+		for (scanf("%d", &N), i = 1; i <= N; ++i) scanf("%d",X+i);
+		vector<int> last, cur;
+		decomp(X[1], last); 
+		for (i = 2; i <= N; ++i) {
+			decomp(X[i], cur);
+			ans += inc(last, cur);
+			last = cur; cur.clear();
+		}
+		printf("Case #%d: %d\n",tc,ans);
+	}
+
+	return 0;
+}
---
+++ GCJ/Roaring Years.py
@@ -0,0 +1,68 @@
+#https://codingcompetitions.withgoogle.com/codejam/round/00000000004362d7/00000000007c0f01
+#math, greedy
+def fact(x, v):
+    while x>0:
+        v.append(x%10)
+        x//=10
+    v.reverse()
+def roar(base,sz):
+    v=[]
+    coin=0
+    while coin<2 or len(v)<sz:
+        sub=[]
+        fact(base,sub)
+        v.extend(sub)
+        base+=1
+        coin+=1
+        
+    z=0
+    for x in v:
+        z=z*10+x
+    return z
+
+for x in range(int(input())):
+    Y=int(input())
+    if Y<12:
+        z=12
+    else:
+        v=[]
+        fact(Y,v)
+        
+        
+        z=roar(1,1+len(v))
+
+        base=1
+        for _ in range(len(v)):
+            base*=10
+            zz=roar(base,1+len(v))
+            z=min(z,zz)
+        base=10
+        while base<=Y:
+            for pad in range(19):
+                if base-pad<=0:
+                    break
+                zz=roar(base-pad,len(v))
+                if zz>Y:
+                    z=min(zz,z)
+            base*=10
+        base=0
+        sz=1
+        for b in v:
+            base=base*10+b
+            zz = roar(base,len(v))
+            if zz>Y:
+                z=min(z,zz)
+            add=1
+            for _ in range(sz+1):
+                for m in range(10):
+                    new_base=base+add*m
+                    new_base//=add
+                    new_base*=add
+                    zz=roar(new_base,len(v))
+                    if zz>Y:
+                        z=min(z,zz)
+                add*=10
+            sz+=1
+    print(f"Case #{x+1}: {z}")
+            
+    
