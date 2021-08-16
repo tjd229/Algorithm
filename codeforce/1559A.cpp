dace07f8868aa50b0a8a5cbd52c914c3bd284720
Cheol Seong Park <tjd4987@naver.com>
2021-08-16 19:58:13 +0900

    math

---
+++ codeforce/1559A.cpp
@@ -0,0 +1,30 @@
+//math
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <memory.h>
+#include <set>
+#include <map>
+#include <queue>
+#include <unordered_map>
+#define ll long long 
+#define pii pair<int,int>
+using namespace std;
+int a[101];
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t; cin >> t;
+	while (t--) {
+		int i, n; cin >> n;
+		for (i = 1; i <= n; ++i) cin >> a[i];
+		int ans = a[1];
+		for (i = 2; i <= n; ++i) ans &= a[i];
+		cout << ans << "\n";
+	}
+ 
+ 
+	return 0;
+}
