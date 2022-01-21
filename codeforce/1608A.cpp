69e2659d3055a555485ad33ca04b04679f1377b9
Cheol Seong Park <tjd4987@naver.com>
2022-01-21 14:01:37 +0900

    implementation

---
+++ codeforce/1608A.cpp
@@ -0,0 +1,16 @@
+//implementation
+#include <string>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		int n; cin >> n;
+		for (int i = 1; i <= n; ++i) cout << i + 1 << " ";
+		cout << "\n";
+		
+	}
+ 
+	return 0;
+}
