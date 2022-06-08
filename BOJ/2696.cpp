6ca516abe6e7a3b9ee66c594fd740792cf64fe01
Cheol Seong Park <tjd4987@naver.com>
2022-06-08 19:52:13 +0900

    bs

--- BOJ/2696.cpp
+++ BOJ/2696.cpp
@@ -1,34 +1,56 @@
-//greedy
+//bs
 #include <stdio.h>
-#include <queue>
+#include <algorithm>
+#include <vector>
+#include <set>
 using namespace std;
+int P[9999];
 int main() {
 	int T;
-	int i, M;
 	for (scanf("%d", &T); T--;) {
-		priority_queue<int> mxh, mnh;
-		scanf("%d",&M);
-		printf("%d\n", (M >> 1) + 1);
-		for (i = 0; i < M; ++i) {
-			int e; scanf("%d",&e);
-			mxh.push(e);
-			if ((i & 1) == 0) {
-				if (mxh.size()>1) {
-					mnh.push(-mxh.top());
-					mxh.pop();
-					while (mxh.top() > -mnh.top()) {
-						mnh.push(-mxh.top()); mxh.pop();
-						mxh.push(-mnh.top()); mnh.pop();
-					}
-				}
-				printf("%d ",mxh.top());
-			}
-			if (i == 19) {
-				i -= 20, M -= 20;
-				puts("");
-			}
+		int i, M;
+		vector<int> ans;
+		set<pair<int,int> > st;
+		for (scanf("%d", &M), i = 0; i < M; ++i) {
+			scanf("%d", P+i);
+			st.insert({P[i],i});
 		}
-		if (i) puts("");
+		auto it = st.begin();
+		for (i = 0; i < M / 2; ++i) it = next(it);
+		i = M - 1;
+		while (i > 1) {
+			ans.push_back(it->first);
+			int l = 0, r = 0;
+			auto p1 = st.lower_bound({ P[i],i }); --i;
+			if (*p1 < *it) ++l;
+			else if (*p1 > *it) ++r;
+
+			auto p2 = st.lower_bound({ P[i],i }); --i;
+			if (*p2 < *it) ++l;
+			else if (*p2 > *it) ++r;
+
+			if (l + r == 2) {
+				st.erase(p1); st.erase(p2);
+				if (l > r)it = next(it);
+				else if (l < r) it = prev(it);
+			}
+			else {
+				auto x = *it;
+				st.erase(p1); st.erase(p2);
+				it = st.upper_bound(x);
+				if (l < r) it = prev(it);
+			}
+			
+		}
+		ans.push_back(it->first);
+		reverse(ans.begin(), ans.end());
+
+		for (printf("%d\n", ans.size()), i = 0; i < ans.size(); ++i) {
+			if (i > 0 && i % 10 == 0) puts("");
+			printf("%d ", ans[i]);
+		}
+		if (i % 10 > 0) puts("");
+
 	}
 	return 0;
 }
